4d30a3dbe41c1cf9b106309f58ca861efa0abc54
Jeremías <jrmsdev@gmail.com>
2025-03-20 16:18:52 -0300

    http: internal static .base alias

--- cmd/clvq/clvq_http.go
+++ cmd/clvq/clvq_http.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func httpHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,38 +26,43 @@ func httpHandler(w http.ResponseWriter, r *http.Request) {
 
 	if ext == ".html" {
 		if _, err := os.Stat(templatePath); os.IsNotExist(err) {
-			log.Printf("404 %s - %v", r.URL.Path, err)
+			log.Printf("404 %s - %v", reqPath, err)
 			http.Error(w, "404 - not found", http.StatusNotFound)
 			return
 		}
 		tmpl, err := tplGet(templatePath)
 		if err != nil {
-			log.Printf("500 %s - %v", r.URL.Path, err)
+			log.Printf("500 %s - %v", reqPath, err)
 			http.Error(w, "500 - Failed to load template", http.StatusInternalServerError)
 			return
 		}
 		data := tplGetData(templatePath)
 		if err := tmpl.Execute(w, data); err != nil {
-			log.Printf("500 %s - %v", r.URL.Path, err)
+			log.Printf("500 %s - %v", reqPath, err)
 			http.Error(w, "500 - Failed to render template", http.StatusInternalServerError)
 			return
 		}
-		log.Printf("200 %s", r.URL.Path)
+		log.Printf("200 %s", reqPath)
 		return
 	}
 
 	// serve static files
 
-	filePath := filepath.Join(".", "static", reqPath)
+	var filePath string
+	if strings.HasPrefix(reqPath, "/.base/") {
+		filePath = filepath.Join("/opt/clvq/src/base/static", strings.TrimPrefix(reqPath, "/.base/"))
+	} else {
+		filePath = filepath.Join(".", "static", reqPath)
+	}
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		log.Printf("404 %s - %v", r.URL.Path, err)
+		log.Printf("404 %s - %v", reqPath, err)
 		http.Error(w, "404 - not found", http.StatusNotFound)
 		return
 	}
 	mimeType := mime.TypeByExtension(ext)
 	w.Header().Set("Content-Type", mimeType)
 	http.ServeFile(w, r, filePath)
-	log.Printf("200 %s", r.URL.Path)
+	log.Printf("200 %s", reqPath)
 }
 
 func httpMain(port string) {
