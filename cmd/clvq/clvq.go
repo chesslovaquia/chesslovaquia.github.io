22989d8658bd821e27a65c83fcd9afa2aced89b4
Jeremías <jrmsdev@gmail.com>
2025-03-23 13:45:54 -0300

    clvq: admin prefix flag

--- cmd/clvq/clvq.go
+++ cmd/clvq/clvq.go
@@ -10,19 +10,22 @@ import (
 
 var (
 	// flags
-	optPort      string
-	optTplDir    string
-	optTplBase   string
-	optTplIndex  string
-	optStaticDir string
+	optPort        string
+	optTplDir      string
+	optTplBase     string
+	optTplIndex    string
+	optStaticDir   string
+	optAdminPrefix string
 )
 
 func main() {
 	flag.StringVar(&optPort, "port", "8044", "HTTP server port")
+
 	flag.StringVar(&optTplDir, "tpl", "tpl", "html template dir path")
-	flag.StringVar(&optTplBase, "b", "base.html", "base html template")
+	flag.StringVar(&optTplBase, "tpl.base", "base.html", "base html template")
 	flag.StringVar(&optTplIndex, "tpl.index", "index.html", "index html filename")
 	flag.StringVar(&optStaticDir, "static", "static", "static dir path")
+	flag.StringVar(&optAdminPrefix, "adm.prefix", "/.adm/", "admin url prefix")
 
 	input := flag.String("i", "", "template file path")
 	output := flag.String("o", "", "html file path")
