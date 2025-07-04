01871953d20a7a1a1ddd8215d18d6281f33bb8fe
Jeremías <jrmsdev@gmail.com>
2025-07-03 22:00:21 -0300

    global lib access

--- clvq/Clvq.ts
+++ clvq/Clvq.ts
@@ -1,7 +1,18 @@
 class Clvq {
+
 	constructor() {
 		console.log('Welcome to chesslovaquia.');
 	}
+
+	public w3ToggleMenu(id: string): void {
+		var x = document.getElementById(id);
+		if (x.className.indexOf("w3-show") === -1) {
+			x.className += " w3-show";
+		} else {
+			x.className = x.className.replace(" w3-show", "");
+		}
+	}
+
 }
 
 export { Clvq };
