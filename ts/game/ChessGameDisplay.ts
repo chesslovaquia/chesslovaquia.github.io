28116b360d3ac5ef2c7b5b1266379290a0aedaa4
Jeremías <jrmsdev@gmail.com>
2025-07-16 00:15:16 -0300

    get semicolons back

--- ts/game/ChessGameDisplay.ts
+++ ts/game/ChessGameDisplay.ts
@@ -1,60 +1,60 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
-import { Chess, Move } from 'chess.js'
+import { Chess, Move } from 'chess.js';
 
-import { ChessGameConfig } from './ChessGameConfig'
-import { ChessGameMove }   from './ChessGameMove'
+import { ChessGameConfig } from './ChessGameConfig';
+import { ChessGameMove }   from './ChessGameMove';
 
 class ChessGameDisplay {
-	private readonly game: Chess
-	private readonly move: ChessGameMove
+	private readonly game: Chess;
+	private readonly move: ChessGameMove;
 
-	private statusElement: HTMLElement | undefined
+	private statusElement: HTMLElement | undefined;
 
 	constructor(cfg: ChessGameConfig, g: Chess, m: ChessGameMove) {
-		this.game          = g
-		this.move          = m
-		this.statusElement = cfg.statusElement
+		this.game          = g;
+		this.move          = m;
+		this.statusElement = cfg.statusElement;
 	}
 
 	public updateStatus(): void {
 		if (!this.statusElement) {
-			return
+			return;
 		}
-		let statusText = ''
+		let statusText = '';
 		if (this.game.isGameOver()) {
 			if (this.game.isCheckmate()) {
-				const winner = this.game.turn() === 'w' ? 'Black' : 'White'
-				statusText = `Checkmate! ${winner} wins.`
+				const winner = this.game.turn() === 'w' ? 'Black' : 'White';
+				statusText = `Checkmate! ${winner} wins.`;
 			} else if (this.game.isDraw()) {
-				statusText = 'Draw!'
+				statusText = 'Draw!';
 			} else if (this.game.isStalemate()) {
-				statusText = 'Stalemate!'
+				statusText = 'Stalemate!';
 			} else if (this.game.isThreefoldRepetition()) {
-				statusText = 'Draw by threefold repetition!'
+				statusText = 'Draw by threefold repetition!';
 			} else if (this.game.isInsufficientMaterial()) {
-				statusText = 'Draw by insufficient material!'
+				statusText = 'Draw by insufficient material!';
 			}
 		} else {
-			const currentPlayer = this.game.turn() === 'w' ? 'White' : 'Black'
-			statusText = `${currentPlayer} to move`
+			const currentPlayer = this.game.turn() === 'w' ? 'White' : 'Black';
+			statusText = `${currentPlayer} to move`;
 			if (this.game.inCheck()) {
-				statusText += ' (in check)'
+				statusText += ' (in check)';
 			} else {
 				if (this.move.isPromotion()) {
-					statusText += ' (pawn promotion)'
+					statusText += ' (pawn promotion)';
 				}
 			}
 		}
-		this.statusElement.textContent = statusText
+		this.statusElement.textContent = statusText;
 	}
 
 	public clear(): void {
 		if (this.statusElement) {
-			this.statusElement.textContent = ''
+			this.statusElement.textContent = '';
 		}
 	}
 }
 
-export { ChessGameDisplay }
+export { ChessGameDisplay };
