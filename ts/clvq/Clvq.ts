06e978a6e9fe2f06911025bec93b727a689f9898
Jeremías <jrmsdev@gmail.com>
2025-07-15 22:22:14 -0300

    relocate main.ts and call async init method

--- ts/clvq/Clvq.ts
+++ ts/clvq/Clvq.ts
@@ -1,21 +1,34 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
+import { ClvqIndexedDB } from './ClvqIndexedDB';
+
+const dbName    = 'clvqDB';
+const dbStore   = 'main';
+const dbVersion = 1;
+
 class Clvq {
+	private readonly db: ClvqIndexedDB;
+
 	constructor() {
-		console.log('Clvq loaded.')
+		console.log('Clvq loaded.');
+		this.db = new ClvqIndexedDB(dbName, dbStore, dbVersion);
+	}
+
+	public async init(): Promise<void> {
+		await this.db.setItem('dbVersion', `${dbVersion}`);
 	}
 
 	public w3ToggleMenu(id: string): void {
-		let x = document.getElementById(id)
+		let x = document.getElementById(id);
 		if (x) {
 			if (x.className.indexOf("w3-show") === -1) {
-				x.className += " w3-show"
+				x.className += " w3-show";
 			} else {
-				x.className = x.className.replace(" w3-show", "")
+				x.className = x.className.replace(" w3-show", "");
 			}
 		} else {
-			console.log('Clvq w3ToggleMenu ERROR:', id, 'not found')
+			console.log('Clvq w3ToggleMenu ERROR:', id, 'not found');
 		}
 	}
 }
