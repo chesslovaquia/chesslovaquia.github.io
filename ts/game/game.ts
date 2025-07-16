d302302f18b3500f03a8281582375d772048ab7b
Jeremías <jrmsdev@gmail.com>
2025-07-16 15:50:22 -0300

    get semicolons back

--- ts/game/game.ts
+++ ts/game/game.ts
@@ -1,17 +1,17 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
-import { ChessGame }       from './ChessGame'
-import { ChessGameConfig } from './ChessGameConfig'
-import { ChessGameError }  from './ChessGameError'
+import { ChessGame }       from './ChessGame';
+import { ChessGameConfig } from './ChessGameConfig';
+import { ChessGameError }  from './ChessGameError';
 
 // Initialize when DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
-	const boardElement = document.getElementById('chessboard')
+	const boardElement = document.getElementById('chessboard');
 	if (boardElement) {
-		const cfg = new ChessGameConfig(boardElement)
-		const game = new ChessGame(cfg)
+		const cfg = new ChessGameConfig(boardElement);
+		const game = new ChessGame(cfg);
 	} else {
-		throw new ChessGameError('board not found!')
+		throw new ChessGameError('board not found!');
 	}
-})
+});
