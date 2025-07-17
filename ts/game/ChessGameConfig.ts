2c177a319711caf972fb2dec666defa616575e4d
Jeremías <jrmsdev@gmail.com>
2025-07-16 22:24:19 -0300

    get semicolons back

--- ts/game/ChessGameConfig.ts
+++ ts/game/ChessGameConfig.ts
@@ -2,23 +2,23 @@
 // See LICENSE file.
 
 class ChessGameConfig {
-	public boardElement: HTMLElement
+	public boardElement: HTMLElement;
 
-	public statusElement: HTMLElement | undefined
+	public statusElement: HTMLElement | undefined;
 
-	public resetButtonLaptop: HTMLElement | undefined
-	public resetButtonMobile: HTMLElement | undefined
+	public resetButtonLaptop: HTMLElement | undefined;
+	public resetButtonMobile: HTMLElement | undefined;
 
-	public mobileScreen: HTMLElement | undefined
-	public laptopScreen: HTMLElement | undefined
+	public mobileScreen: HTMLElement | undefined;
+	public laptopScreen: HTMLElement | undefined;
 
 	constructor(boardElement: HTMLElement) {
-		this.boardElement  = boardElement
-		this.statusElement = document.getElementById('gameStatus') || undefined
+		this.boardElement  = boardElement;
+		this.statusElement = document.getElementById('gameStatus') || undefined;
 
-		this.resetButtonLaptop = document.getElementById('gameResetLaptop')  || undefined
-		this.resetButtonMobile = document.getElementById('gameResetMobile')  || undefined
+		this.resetButtonLaptop = document.getElementById('gameResetLaptop')  || undefined;
+		this.resetButtonMobile = document.getElementById('gameResetMobile')  || undefined;
 	}
 }
 
-export { ChessGameConfig }
+export { ChessGameConfig };
