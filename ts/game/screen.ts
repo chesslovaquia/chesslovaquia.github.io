0d1845abffdc156a42840786fb20183e7718bc85
Jeremías <jrmsdev@gmail.com>
2025-07-21 19:23:23 -0300

    improve toggle board

--- ts/game/screen.ts
+++ ts/game/screen.ts
@@ -4,22 +4,30 @@
 import { ChessGameError  } from './ChessGameError';
 
 export function toggleScreen(mode: string) {
+
 	console.debug('Toggle screen:', mode);
+
 	const cgWrap       = document.getElementById('chessboard');
 	const mobileScreen = document.getElementById('chessBoardMobile');
 	const laptopScreen = document.getElementById('chessBoardLaptop');
+
+	const toggleBoard = (parent: HTMLElement | null) => {
+		if (parent) {
+			setTimeout(() => {
+				parent.appendChild(cgWrap as Node);
+			}, 0);
+		}
+	}
+
 	if (cgWrap && mobileScreen && laptopScreen) {
 		if (mode === 'mobile') {
-			setTimeout(() => {
-				mobileScreen?.appendChild(cgWrap as Node);
-			}, 0);
+			toggleBoard(mobileScreen);
 		} else {
-			setTimeout(() => {
-				laptopScreen?.appendChild(cgWrap as Node);
-			}, 0);
+			toggleBoard(laptopScreen);
 		}
 	} else {
 		const msg = `Toggle screen divs not found: cg-wrap=${cgWrap} mobile=${mobileScreen} laptop=${laptopScreen}`;
 		throw new ChessGameError(msg);
 	}
+
 }
