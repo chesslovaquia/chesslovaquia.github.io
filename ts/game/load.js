601c6cdfc18b5713751819263a7bbd5a50eaba60
Jeremías <jrmsdev@gmail.com>
2025-07-24 17:14:46 -0300

    call game assets loader from home page

--- ts/game/load.js
+++ ts/game/load.js
@@ -1,9 +1,12 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
+console.debug('Game assets load.');
+
 {{- $cdn := site.Params.cdn }}
 {{- with site.Params.game_assets }}
 	{{- range . }}
+console.debug("Fetch: {{ $cdn }}/{{ . }}");
 fetch("{{ $cdn }}/{{ . }}");
 	{{- end }}
 {{- end }}
