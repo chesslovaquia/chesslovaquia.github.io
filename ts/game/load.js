21de83114ecac948fd1b336b5fa222b36a224225
Jeremías <jrmsdev@gmail.com>
2025-07-24 16:52:02 -0300

    game assets loader

---
+++ ts/game/load.js
@@ -0,0 +1,10 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+{{- $cdn := site.Params.cdn }}
+
+{{- with site.Params.game_assets }}
+	{{- range . }}
+fetch("{{ $cdn }}/{{ . }}");
+	{{- end }}
+{{- end }}
