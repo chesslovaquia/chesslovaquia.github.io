da847a4806ddc55b7a4a3810d015674b261893cd
Jeremías <jrmsdev@gmail.com>
2025-07-24 17:24:51 -0300

    game load assets build id

--- ts/game/load.js
+++ ts/game/load.js
@@ -1,12 +1,16 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
-
 console.debug('Game assets load.');
 
 {{- $cdn := site.Params.cdn }}
+console.debug('Assets CDN: {{ $cdn }}');
+
+{{- $buildID := getenv "HUGO_CLVQ_BUILD" | default "UNSET" }}
+console.debug('Assets build ID: {{ $buildID }}');
+
 {{- with site.Params.game_assets }}
 	{{- range . }}
-console.debug("Fetch: {{ $cdn }}/{{ . }}");
-fetch("{{ $cdn }}/{{ . }}");
+console.debug('Fetch: {{ . }}');
+fetch('{{ $cdn }}/{{ . }}?v={{ $buildID }}');
 	{{- end }}
 {{- end }}
