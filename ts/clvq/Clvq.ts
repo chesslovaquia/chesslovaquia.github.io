19d2d15eb978fbfe50e3f9bc24e3da3b92899a12
Jeremías <jrmsdev@gmail.com>
2025-08-07 16:57:42 -0300

    remove clvq db init

--- ts/clvq/Clvq.ts
+++ ts/clvq/Clvq.ts
@@ -1,22 +1,9 @@
 // Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 // See LICENSE file.
 
-import { ClvqIndexedDB } from './ClvqIndexedDB';
-
-const dbName    = 'clvqDB';
-const dbStore   = 'main';
-const dbVersion = 1;
-
 class Clvq {
-	private readonly db: ClvqIndexedDB;
-
 	constructor() {
 		console.debug('Clvq loaded.');
-		this.db = new ClvqIndexedDB(dbName, dbStore, dbVersion);
-	}
-
-	public async init(): Promise<void> {
-		await this.db.setItem('dbVersion', `${dbVersion}`);
 	}
 
 	public w3ToggleMenu(id: string): void {
