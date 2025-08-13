dfa9fd8bd39c796b225970650a45e12e7f6522d8
Jeremías <jrmsdev@gmail.com>
2025-08-13 18:58:50 -0300

    game menu hide modal

--- ts/clvq/Clvq.ts
+++ ts/clvq/Clvq.ts
@@ -9,13 +9,22 @@ class Clvq {
 	public w3ToggleMenu(id: string): void {
 		let x = document.getElementById(id);
 		if (x) {
-			if (x.className.indexOf("w3-show") === -1) {
-				x.className += " w3-show";
+			if (x.classList.contains("w3-show")) {
+				x.classList.toggle('w3-show', false);
 			} else {
-				x.className = x.className.replace(" w3-show", "");
+				x.classList.toggle('w3-show', true);
 			}
 		} else {
 			console.log('Clvq w3ToggleMenu ERROR:', id, 'not found');
+		}
+	}
+
+	public w3HideModal(id: string): void {
+		let x = document.getElementById(id);
+		if (x) {
+			x.classList.toggle('w3-show', false);
+		} else {
+			console.log('Clvq w3HideModal ERROR:', id, 'not found');
 		}
 	}
 }
