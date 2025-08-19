098804498bb48cd5aebbc7dccdd04611fa9e5986
Jeremías <jrmsdev@gmail.com>
2025-08-19 12:37:34 -0300

    fix board assets service worker url

--- ts/clvq/sw.js
+++ ts/clvq/sw.js
@@ -42,7 +42,7 @@ async function installHandler() {
 		const cache = await caches.open(CACHE_NAME);
 		const cache_urls = mergeUnique(
 			SITE_URLS.map(s => `${BASE_URL}${s}`),
-			BOARD_ASSETS.map(s => `${BASE_URL}${s}`),
+			BOARD_ASSETS.map(s => `${BASE_URL}/${s}`),
 			assets_urls.map(s => `${ASSETS_CDN}${s}?v=${BUILD_ID}`)
 		);
 		await cache.addAll(cache_urls);
