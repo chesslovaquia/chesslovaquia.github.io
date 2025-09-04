c856c6c9530c408211c7a419c4fae41a4f8cc564
Jeremías <jrmsdev@gmail.com>
2025-09-04 00:07:03 -0300

    silent vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		environment: 'happy-dom',
+		silent: true,
 		include: ['ts/testing/**/*_test.ts'],
 		exclude: [
 			'node_modules/**',
