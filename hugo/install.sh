51e42a1f9925762a5d0a273c6d205dd43f2e0b06
Jeremías <jrmsdev@gmail.com>
2025-09-10 12:24:09 -0300

    shellcheck fix hugo/install.sh

--- hugo/install.sh
+++ hugo/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -eu
 HUGO_VERSION=$(cat hugo/VERSION)
+HUGO_URL='https://github.com/gohugoio/hugo/releases/download'
 wget -q -O /tmp/hugo.deb \
-	https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
+	"${HUGO_URL}/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb"
 dpkg -i /tmp/hugo.deb
 rm -f /tmp/hugo.deb
 exit 0
