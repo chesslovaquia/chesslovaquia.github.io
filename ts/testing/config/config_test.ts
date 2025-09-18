42702d22c1bdf33275a621fede9d51f4e96797d6
Jeremías <jrmsdev@gmail.com>
2025-09-18 19:01:12 -0300

    test config game ui validate

--- ts/testing/config/config_test.ts
+++ ts/testing/config/config_test.ts
@@ -70,3 +70,60 @@ describe('ConfigGameUI', () => {
 		new ConfigGameUI(board);
 	});
 });
+
+describe('ConfigGameUI error', () => {
+	test('gameDescription', () => {
+		document.getElementById('gameDescription')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+	test('gameStatus', () => {
+		document.getElementById('gameStatus')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+	test('gameOutcome', () => {
+		document.getElementById('gameOutcome')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+	test('gameReset', () => {
+		document.getElementById('gameReset')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+	test('gameNavBackward', () => {
+		document.getElementById('gameNavBackward')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+	test('gameNavForward', () => {
+		document.getElementById('gameNavForward')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+	test('gameFlipBoard', () => {
+		document.getElementById('gameFlipBoard')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+	test('gameNavFirstMove', () => {
+		document.getElementById('gameNavFirstMove')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+	test('gameNavLastMove', () => {
+		document.getElementById('gameNavLastMove')?.remove();
+		expect(() => {
+			new ConfigGameUI(board);
+		}).toThrow(ConfigError);
+	});
+});
