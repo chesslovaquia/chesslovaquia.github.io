ee9b6800809b8004832e3fd58d4fdc886cd79bdc
Jeremías <jrmsdev@gmail.com>
2025-09-19 12:16:57 -0300

    testing mock db open

---
+++ ts/testing/clvq/db_test.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+import { test, expect } from 'vitest';
+
+import { ClvqIndexedDB, Store } from '../../clvq/ClvqIndexedDB';
+
+test('open', () => {
+	const db = new ClvqIndexedDB(Store.state);
+	expect(db.store).toBe('state');
+});
